a940574a26829f3b5ddbba08d9d8ab1959df4ffb
Chris Yoon <chy.yoon@gmail.com>
2021-08-19 14:41:07 -0700

    added approval card componenet for back end comment approval

--- buttons/src/index.js
+++ buttons/src/index.js
@@ -1,29 +1,42 @@
 import React from "react";
 import ReactDom from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail
-        author='Pam'
-        timeAgo='Today at 4'
-        text='hell yeah'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Jason'
-        timeAgo='Today at 3'
-        text='hell no'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Carlsbad'
-        timeAgo='tofay'
-        text='hell OK'
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+        </div>
+        Are you sure?
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Pam'
+          timeAgo='Today at 4'
+          text='hell yeah'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jason'
+          timeAgo='Today at 3'
+          text='hell no'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Carlsbad'
+          timeAgo='tofay'
+          text='hell OK'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
