69f4981eb88bf3062209423e1cf35604ed267f80
Chris Yoon <chy.yoon@gmail.com>
2021-09-08 17:32:27 -0700

    added conditional returns

--- weather/src/index.js
+++ weather/src/index.js
@@ -5,14 +5,40 @@ import ReactDOM from "react-dom";
 
 //   return <div>Latitude: </div>;
 // };
+// const App = () => {
+//};
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitude: </div>;
+    //constructor function we can define. in a js class it is the first function to be called anytime an instance of the class is
+  }
+  render() {
+    // always need a render. must have render method in components.
+
+    // <div>
+    //   Latitude: {this.state.lat}
+    //   <br />
+    //   Error: {this.state.errorMessage}
+    // </div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat} </div>;
+    }
+    return <div> Loading!</div>;
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
