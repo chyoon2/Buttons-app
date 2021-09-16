4a97ad116658664d3fb9cc9ed53defa86d9500f4
Chris Yoon <chy.yoon@gmail.com>
2021-09-16 13:32:27 -0700

    added component and refactored list

--- pics/src/components/ImageList/ImageList.js
+++ pics/src/components/ImageList/ImageList.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "../ImageList.css";
+import ImageCard from "../ImageCard";
 
 const ImageList = (props) => {
   // const images = props.fatimages.map((arg) => {
   //   return <img alt={arg.description} key={arg.id} src={arg.urls.regular} />;
   // });  destructured refactor below
-  const images = props.fatimages.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />;
+  const images = props.fatimages.map((arg) => {
+    return <ImageCard key={arg.id} image={arg} />;
   });
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
