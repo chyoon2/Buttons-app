d6616b58852c6952ccdabf94d5efc992fb6aa547
Chris Yoon <chy.yoon@gmail.com>
2021-09-30 12:22:22 -0700

    added a component that translates english to other languages, uses google api

--- widgets/src/Components/App.js
+++ widgets/src/Components/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
 import Dropdown from "./Dropdown";
+import Translate from "./Translate";
+import Convert from "./Convert";
 
 const items = [
   { title: "what is React", content: "react is a front end js framework" },
@@ -15,21 +17,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        ToggleDropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
