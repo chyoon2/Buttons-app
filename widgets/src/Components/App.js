dc35c15d6a83eb536f845d96b6d9265f6c68e989
Chris Yoon <chy.yoon@gmail.com>
2021-09-29 18:23:43 -0700

    added a dropdown list

--- widgets/src/Components/App.js
+++ widgets/src/Components/App.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 const items = [
   { title: "what is React", content: "react is a front end js framework" },
   { title: "why", content: "react is a fav" },
   { title: "how?", content: "by components" },
 ];
+const options = [
+  { label: "the color red", value: "red" },
+  { label: "the color green", value: "green" },
+  { label: "the color pink", value: "pink" },
+];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
-      <Search></Search>
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        ToggleDropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
-{
-  /* <Accordion items={items}></Accordion> */
-}
